3a6a857ff11436e38a09c5637af3cae0cd74f9ec
Anton Nakanechny <kashot.kukuskin@gmail.com>
2026-03-25 14:15:02 -0300

    доьавить запуск через https для локальной разработки

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import path from 'node:path';
 
 import { svelte } from '@sveltejs/vite-plugin-svelte';
+import basicSsl from '@vitejs/plugin-basic-ssl';
 import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [svelte(), basicSsl()],
   resolve: {
     alias: {
       '@': path.resolve(import.meta.dirname, 'src'),
